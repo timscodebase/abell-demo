bf20545edb838d06c5935de0bad6e232dbb55da9
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-10-06 19:01:07 -0700

    pnpm

--- _components/types.ts
+++
@@ -1,2 +0,0 @@
-export type LevelType = 1 | 2 | 3 | 4 | 5 | 6
-export type TextType = string
--- client-assets/index-client.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#counter-button').addEventListener('click', () => {
-  const countEl = document.querySelector('#count');
-  countEl.innerText = Number(countEl.innerText) + 1;
-});
